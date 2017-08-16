15d872deaeaaaacfdb40e07b179ad8d2c0d17955
ykymd <y.yamada6426@gmail.com>
2017-08-16 21:27:08 +0900

    Initial Commit

---
+++ glfw-game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  glfw-game
+//
+//  Created by Yuki Yamada on 2017/08/16.
+//  Copyright © 2017年 yukiyama. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
